6dece440ac80ab12c18a43747ab7a9585101d360
WendyJackson1 <wendyrobinh@gmail.com>
2022-05-12 23:51:48 -0400

    coffee

---
+++ MVCIntro/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCIntro.Models;
+using System.Diagnostics;
+
+namespace MVCIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult RegistrationForm()
+        {
+            return View();
+        }
+
+        public IActionResult Registration(Registation Ur)
+        {
+            return View(Ur);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCIntro/Models/Resgistation.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCIntro.Models
+{
+    public class Registation
+    {
+        public string Firstname { get; set; }
+        public string lastname { get; set; }
+        public string email{ get; set; }
+        public string Password { get; set; }
+
+    }
+}
